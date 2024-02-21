0d3b2bef471cb0f0092c43849486d83ee62ee75a
Vladimir Teplov <vladimir.teplov@gmail.com>
2024-02-21 12:55:56 +0200

    add user unit tests

--- server/models/user_unit_test.go
+++ server/models/user_unit_test.go
@@ -1,14 +1,8 @@
 package models
 
 import (
-	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
-	"github.com/vavilen84/nft-project/constants"
-	"github.com/vavilen84/nft-project/validation"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"log"
 	"testing"
 )
 
@@ -55,207 +49,160 @@ func Test_ForgotPassword_ok(t *testing.T) {
 	}
 }
 
-func TestUser_CreateScenario_nicknameValidation(t *testing.T) {
-	db, mock, err := sqlmock.New()
+func Test_SetEmailTwoFaCode_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
 	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		panic(err)
 	}
 	defer db.Close()
-	gormDB, err := gorm.Open(mysql.New(mysql.Config{
-		SkipInitializeWithVersion: true,
-		Conn:                      db,
-	}), &gorm.Config{})
+	gormDB := GetMockDB(db)
 
-	// should be error
-	m := User{
-		//Nickname: "",
-	}
-	err = InsertUser(gormDB, &m)
-	v, ok := err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
-	assert.Equal(t, fmt.Sprintf(constants.MinValueErrorMsg, "Nickname", "3"), v["Nickname"][0].Message)
+	sql := "UPDATE users"
+	sqlMock.ExpectExec(sql).WillReturnResult(sqlmock.NewResult(0, 1))
 
-	//m.Nickname = "valid.nickname"
-	err = InsertUser(gormDB, &m)
-	v, ok = err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
+	m := GetTestValidUserModel()
+	err = SetEmailTwoFaCode(gormDB, &m)
+	assert.Nil(t, err)
 
-	// no error
-	_, ok = v["Nickname"]
-	assert.False(t, ok)
-
-	if err := mock.ExpectationsWereMet(); err != nil {
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
 
-func TestUser_CreateScenario_passwordValidation(t *testing.T) {
-	db, mock, err := sqlmock.New()
+func Test_ResetEmailTwoFaCode_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
 	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		panic(err)
 	}
 	defer db.Close()
-	gormDB, err := gorm.Open(mysql.New(mysql.Config{
-		SkipInitializeWithVersion: true,
-		Conn:                      db,
-	}), &gorm.Config{})
+	gormDB := GetMockDB(db)
 
-	// should be error
-	m := User{
-		Password: "1234567",
-	}
-	err = InsertUser(gormDB, &m)
-	v, ok := err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
-	assert.Equal(t, fmt.Sprintf(constants.MinValueErrorMsg, "Password", "8"), v["Password"][0].Message)
+	sql := "UPDATE users"
+	sqlMock.ExpectExec(sql).WillReturnResult(sqlmock.NewResult(0, 1))
 
-	// should be error
-	m = User{
-		Password: "1234567+",
-	}
-	err = InsertUser(gormDB, &m)
-	v, ok = err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
-	assert.Equal(t, fmt.Sprintf(constants.CustomPasswordValidatorTagErrorMsg), v["Password"][0].Message)
+	m := GetTestValidUserModel()
+	err = ResetEmailTwoFaCode(gormDB, &m)
+	assert.Nil(t, err)
 
-	// no error
-	rowValidPassword := "12345678lT*"
-	m.Password = rowValidPassword
-	err = InsertUser(gormDB, &m)
-	v, ok = err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
-	_, ok = v["Password"]
-	assert.False(t, ok)
-
-	if err := mock.ExpectationsWereMet(); err != nil {
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
 
-func TestUser_CreateScenario_roleValidation(t *testing.T) {
-	db, mock, err := sqlmock.New()
+func Test_ResetResetPasswordToken_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
 	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		panic(err)
 	}
 	defer db.Close()
-	gormDB, err := gorm.Open(mysql.New(mysql.Config{
-		SkipInitializeWithVersion: true,
-		Conn:                      db,
-	}), &gorm.Config{})
+	gormDB := GetMockDB(db)
 
-	// should be error
-	m := User{
-		Role: 0,
-	}
-	err = InsertUser(gormDB, &m)
-	v, ok := err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "Role"), v["Role"][0].Message)
+	sql := "UPDATE users"
+	sqlMock.ExpectExec(sql).WillReturnResult(sqlmock.NewResult(0, 1))
 
-	// should be error
-	m = User{
-		Role: 2,
-	}
-	err = InsertUser(gormDB, &m)
-	v, ok = err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
-	assert.Equal(t, fmt.Sprintf(constants.LowerThanTagErrorMsg, "2"), v["Role"][0].Message)
+	m := GetTestValidUserModel()
+	err = ResetResetPasswordToken(gormDB, &m)
+	assert.Nil(t, err)
 
-	m.Role = constants.RoleUser
-	err = InsertUser(gormDB, &m)
-	v, ok = err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
-
-	// no error
-	_, ok = v["Role"]
-	assert.False(t, ok)
-
-	if err := mock.ExpectationsWereMet(); err != nil {
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
 
-func TestUser_CreateScenario_2FaCodeValidation(t *testing.T) {
-	db, mock, err := sqlmock.New()
+func Test_SetUserEmailVerified_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
 	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		panic(err)
 	}
 	defer db.Close()
-	gormDB, err := gorm.Open(mysql.New(mysql.Config{
-		SkipInitializeWithVersion: true,
-		Conn:                      db,
-	}), &gorm.Config{})
+	gormDB := GetMockDB(db)
 
-	// should be error
-	m := User{
-		EmailTwoFaCode: "",
-	}
-	err = InsertUser(gormDB, &m)
-	v, ok := err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "EmailTwoFaCode"), v["EmailTwoFaCode"][0].Message)
+	sql := "UPDATE users"
+	sqlMock.ExpectExec(sql).WillReturnResult(sqlmock.NewResult(0, 1))
 
-	m.EmailTwoFaCode = "123456"
-	err = InsertUser(gormDB, &m)
-	v, ok = err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
-	}
+	m := GetTestValidUserModel()
+	m.IsEmailVerified = true
+	m.EmailTwoFaCode = ""
+	err = SetUserEmailVerified(gormDB, &m)
+	assert.Nil(t, err)
 
-	// no error
-	_, ok = v["EmailTwoFaCode"]
-	assert.False(t, ok)
-
-	if err := mock.ExpectationsWereMet(); err != nil {
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
 
-func TestUser_CreateScenario_OkInsert(t *testing.T) {
-	//db, mock, err := sqlmock.New()
-	//if err != nil {
-	//	t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	//}
-	//defer db.Close()
-	//gormDB, err := gorm.Open(mysql.New(mysql.Config{
-	//	SkipInitializeWithVersion: true,
-	//	Conn:                      db,
-	//}), &gorm.Config{})
-	//
-	//mock.ExpectBegin()
-	//mock.ExpectExec("INSERT INTO `user`").WillReturnResult(sqlmock.NewResult(1, 1))
-	//mock.ExpectCommit()
-	//
-	//// no error
-	//m := User{
-	//	//Nickname:       "nick",
-	//	Email:          "valid.email@example.com",
-	//	Password:       "12345678lT*",
-	//	Role:           constants.RoleUser,
-	//	EmailTwoFaCode: "123456",
-	//}
-	//err = InsertUser(gormDB, &m)
-	//assert.Nil(t, err)
-	//
-	//if err := mock.ExpectationsWereMet(); err != nil {
-	//	t.Errorf("there were unfulfilled expectations: %s", err)
-	//}
+func Test_UserResetPassword_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	gormDB := GetMockDB(db)
+
+	sql := "UPDATE users"
+	sqlMock.ExpectExec(sql).WillReturnResult(sqlmock.NewResult(0, 1))
+
+	m := GetTestValidUserModel()
+	err = UserResetPassword(gormDB, &m)
+	assert.Nil(t, err)
+
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func Test_UserChangePassword_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	gormDB := GetMockDB(db)
+
+	sql := "UPDATE users"
+	sqlMock.ExpectExec(sql).WillReturnResult(sqlmock.NewResult(0, 1))
+
+	m := GetTestValidUserModel()
+	err = UserChangePassword(gormDB, &m)
+	assert.Nil(t, err)
+
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func Test_encodePassword(t *testing.T) {
+	m := GetTestValidUserModel()
+	pass := m.Password
+	m.encodePassword()
+	assert.NotEqual(t, pass, m.Password)
+	assert.NotEmpty(t, m.PasswordSalt)
+}
+
+func Test_FindUserById_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	gormDB := GetMockDB(db)
+
+	rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+	expectedSQL := "SELECT * FROM `users`"
+	sqlMock.ExpectQuery(expectedSQL).WillReturnRows(rows)
+
+	m, err := FindUserById(gormDB, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, m.Id)
+
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 }
