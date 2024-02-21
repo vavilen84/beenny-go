4e9e463374f6cfcac8f85dbfa2669993511dde36
Vladimir Teplov <vladimir.teplov@gmail.com>
2024-02-21 13:02:44 +0200

    add user unit tests

--- server/models/user_unit_test.go
+++ server/models/user_unit_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestUser_InsertUser_ok(t *testing.T) {
@@ -205,4 +206,79 @@ func Test_FindUserById_ok(t *testing.T) {
 	if err := sqlMock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
+}
+
+func Test_FindUserByTwoFAToken_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	gormDB := GetMockDB(db)
+
+	rows := sqlmock.NewRows([]string{"id", "email_two_fa_code"}).AddRow(1, "123456")
+	expectedSQL := "SELECT * FROM `users`"
+	sqlMock.ExpectQuery(expectedSQL).WillReturnRows(rows)
+
+	m, err := FindUserByTwoFAToken(gormDB, "123456")
+	assert.Nil(t, err)
+	assert.Equal(t, 1, m.Id)
+
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func Test_FindUserByEmail_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	gormDB := GetMockDB(db)
+
+	rows := sqlmock.NewRows([]string{"id", "email"}).AddRow(1, "user@example.com")
+	expectedSQL := "SELECT * FROM `users`"
+	sqlMock.ExpectQuery(expectedSQL).WillReturnRows(rows)
+
+	m, err := FindUserByEmail(gormDB, "user@example.com")
+	assert.Nil(t, err)
+	assert.Equal(t, 1, m.Id)
+
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func Test_FindUserByResetPasswordToken_ok(t *testing.T) {
+	customMatcher := CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	gormDB := GetMockDB(db)
+
+	future := time.Now().Add(1 * time.Hour)
+	rows := sqlmock.NewRows([]string{"id", "password_reset_token", "password_reset_token_expire_at"}).
+		AddRow(1, "123456", &future)
+	expectedSQL := "SELECT * FROM `users`"
+	sqlMock.ExpectQuery(expectedSQL).WillReturnRows(rows)
+
+	m, err := FindUserByResetPasswordToken(gormDB, "123456")
+	assert.Nil(t, err)
+	assert.Equal(t, 1, m.Id)
+
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func Test_SetForgotPasswordData_ok(t *testing.T) {
+	u := User{}
+	u.SetForgotPasswordData()
+	assert.NotEmpty(t, u.PasswordResetToken)
+	assert.NotEmpty(t, u.PasswordResetTokenExpireAt)
 }
