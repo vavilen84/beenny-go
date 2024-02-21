d64fcebb3fa789e53de08097d4bab006d965a6c9
Vladimir Teplov <vladimir.teplov@gmail.com>
2024-02-21 13:44:46 +0200

    add auth jwt unit tests

--- server/auth/auth_unit_test.go
+++ server/auth/auth_unit_test.go
@@ -1,8 +1,11 @@
 package auth
 
 import (
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
+	"github.com/vavilen84/beenny-go/mocks"
 	"github.com/vavilen84/beenny-go/models"
+	"github.com/vavilen84/beenny-go/store"
 	"log"
 	"testing"
 )
@@ -46,22 +49,53 @@ func Test_getJWTInfo(t *testing.T) {
 }
 
 func Test_insertJWTInfo_ok(t *testing.T) {
-	//customMatcher := mocks.CustomMatcher{}
-	//db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer db.Close()
-	//gormDB := store.GetMockDB(db)
-	//
-	//sql := "INSERT INTO `users`"
-	//sqlMock.ExpectExec(sql).WillReturnResult(sqlmock.NewResult(0, 1))
-	//
-	//m := models.GetTestValidUserModel()
-	//err = InsertUser(gormDB, &m)
-	//assert.Nil(t, err)
-	//
-	//if err := sqlMock.ExpectationsWereMet(); err != nil {
-	//	t.Errorf("there were unfulfilled expectations: %s", err)
-	//}
+	customMatcher := mocks.CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	gormDB := store.GetMockDB(db)
+
+	sql := "INSERT INTO `jwt_info`"
+	sqlMock.ExpectExec(sql).WillReturnResult(sqlmock.NewResult(2, 1))
+
+	m := models.User{
+		Id: 15,
+	}
+	jwtInfo, err := insertJWTInfo(gormDB, &m)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, jwtInfo.Id)
+
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func Test_CreateJWT_ok(t *testing.T) {
+	customMatcher := mocks.CustomMatcher{}
+	db, sqlMock, err := sqlmock.New(sqlmock.QueryMatcherOption(customMatcher))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	gormDB := store.GetMockDB(db)
+
+	sql := "INSERT INTO `jwt_info`"
+	sqlMock.ExpectExec(sql).WillReturnResult(sqlmock.NewResult(2, 1))
+
+	m := models.User{
+		Id: 15,
+	}
+	token, err := CreateJWT(gormDB, &m)
+	assert.Nil(t, err)
+	jwtPayload, err := ParseJWTPayload(token)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	assert.Equal(t, 2, jwtPayload.JWTInfoId)
+
+	if err := sqlMock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 }
