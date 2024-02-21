f323964268fddb322a323645b26c039602d06eb5
Vladimir Teplov <vladimir.teplov@gmail.com>
2024-02-21 11:48:50 +0200

    add ses tests

--- server/aws/ses_test.go
+++ server/aws/ses_test.go
@@ -16,5 +16,24 @@ func Test_SendResetPasswordEmail(t *testing.T) {
 	assert.Nil(t, err)
 	mockSESclient.AssertCalled(t, "SendEmail", mock.Anything)
 	mockSESclient.AssertExpectations(t)
+}
 
+func Test_SendEmailVerificationMail(t *testing.T) {
+	mockSESclient := mocks.NewSESClient(t)
+	SetSESClient(mockSESclient)
+	mockSESclient.On("SendEmail", mock.Anything).Return(&ses.SendEmailOutput{}, nil)
+	err := SendEmailVerificationMail("user@example.com", "123456")
+	assert.Nil(t, err)
+	mockSESclient.AssertCalled(t, "SendEmail", mock.Anything)
+	mockSESclient.AssertExpectations(t)
+}
+
+func Test_SendLoginTwoFaCode(t *testing.T) {
+	mockSESclient := mocks.NewSESClient(t)
+	SetSESClient(mockSESclient)
+	mockSESclient.On("SendEmail", mock.Anything).Return(&ses.SendEmailOutput{}, nil)
+	err := SendEmailVerificationMail("user@example.com", "123456")
+	assert.Nil(t, err)
+	mockSESclient.AssertCalled(t, "SendEmail", mock.Anything)
+	mockSESclient.AssertExpectations(t)
 }
