18249d6b8a3b819b971a81d48118f5676269ecdd
Vladimir Teplov <vladimir.teplov@gmail.com>
2024-02-21 14:36:35 +0200

    refactoring env

--- server/env/env.go
+++ server/env/env.go
@@ -5,3 +5,7 @@ import "os"
 func IsTestEnv() bool {
 	return os.Getenv("APP_ENV") == "test"
 }
+
+func IsDevelopmentEnv() bool {
+	return os.Getenv("APP_ENV") == "development"
+}
--- server/store/db.go
+++ server/store/db.go
@@ -2,9 +2,11 @@ package store
 
 import (
 	"database/sql"
+	"github.com/vavilen84/beenny-go/env"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"os"
+	"time"
 )
 
 var (
@@ -43,5 +45,26 @@ func GetMockDB(db *sql.DB) (gormDB *gorm.DB) {
 	}
 	gormDB = gormDB.Session(&gorm.Session{SkipDefaultTransaction: true})
 
+	return
+}
+
+func processInitDb(DbDsn string) (db *gorm.DB) {
+	db, err := gorm.Open(mysql.Open(DbDsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to database: " + err.Error())
+	}
+	if env.IsDevelopmentEnv() {
+		db.Debug()
+	}
+	sqlDB, err := db.DB()
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
 	return
 }
