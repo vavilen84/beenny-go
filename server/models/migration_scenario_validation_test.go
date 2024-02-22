c1a96e975fac515989062b100ef7c9bd5225525d
Vladimir Teplov <vladimir.teplov@gmail.com>
2024-02-22 14:49:54 +0200

    add migration tests scenario on create

--- server/models/migration_scenario_validation_test.go
+++ server/models/migration_scenario_validation_test.go
@@ -1,21 +1,32 @@
 package models
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/vavilen84/beenny-go/constants"
+	"github.com/vavilen84/beenny-go/helpers"
+	"github.com/vavilen84/beenny-go/validation"
 	"testing"
 )
 
-func TestMigration_ValidateOnCreate(t *testing.T) {
-	//u := Migration{}
-	//err := validation.ValidateByScenario(constants.ScenarioCreate, u)
-	////v, ok := err.(validation.Errors)
-	////if !ok {
-	////	log.Fatalln("can not assert validation.Errors")
-	////}
-	//mustHaveErrors := []string{
-	//	fmt.Sprintf(constants.RequiredErrorMsg, "Version"),
-	//	fmt.Sprintf(constants.RequiredErrorMsg, "Filename"),
-	//	fmt.Sprintf(constants.RequiredErrorMsg, "CreatedAt"),
-	//}
-	//ok := helpers.AllStringsAreErrors(mustHaveErrors, err)
-	//assert.True(t, ok)
+func Test_Unit_Migration_ValidateOnCreate_notOk(t *testing.T) {
+	u := Migration{}
+	errs := validation.ValidateByScenario(constants.ScenarioCreate, u)
+	mustHaveErrors := []string{
+		fmt.Sprintf(constants.RequiredErrorMsg, "Version"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "Filename"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "CreatedAt"),
+	}
+	ok := helpers.AllErrorsExist(mustHaveErrors, errs)
+	assert.True(t, ok)
+}
+
+func Test_Unit_Migration_ValidateOnCreate_ok(t *testing.T) {
+	u := Migration{
+		Version:   1708526378,
+		Filename:  "1708526378_add_users_table.up.sql",
+		CreatedAt: 1708526378,
+	}
+	errs := validation.ValidateByScenario(constants.ScenarioCreate, u)
+	assert.Empty(t, errs)
 }
