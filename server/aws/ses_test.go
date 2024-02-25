84cb39f0f24de540aab0603a5cff2ed00edc99db
Vladimir Teplov <vladimir.teplov@gmail.com>
2024-02-25 15:38:54 +0200

    add ses negative unit test

--- server/aws/ses_test.go
+++ server/aws/ses_test.go
@@ -1,18 +1,21 @@
-package aws
+package aws_test
 
 import (
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ses"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/vavilen84/beenny-go/aws"
 	"github.com/vavilen84/beenny-go/mocks"
+	"github.com/vavilen84/beenny-go/test"
 	"testing"
 )
 
 func Test_SendResetPasswordEmail(t *testing.T) {
 	mockSESclient := mocks.NewSESClient(t)
-	SetSESClient(mockSESclient)
+	aws.SetSESClient(mockSESclient)
 	mockSESclient.On("SendEmail", mock.Anything).Return(&ses.SendEmailOutput{}, nil)
-	err := SendResetPasswordEmail("user@example.com", "123456")
+	err := aws.SendResetPasswordEmail("user@example.com", "123456")
 	assert.Nil(t, err)
 	mockSESclient.AssertCalled(t, "SendEmail", mock.Anything)
 	mockSESclient.AssertExpectations(t)
@@ -20,9 +23,9 @@ func Test_SendResetPasswordEmail(t *testing.T) {
 
 func Test_SendEmailVerificationMail(t *testing.T) {
 	mockSESclient := mocks.NewSESClient(t)
-	SetSESClient(mockSESclient)
+	aws.SetSESClient(mockSESclient)
 	mockSESclient.On("SendEmail", mock.Anything).Return(&ses.SendEmailOutput{}, nil)
-	err := SendEmailVerificationMail("user@example.com", "123456")
+	err := aws.SendEmailVerificationMail("user@example.com", "123456")
 	assert.Nil(t, err)
 	mockSESclient.AssertCalled(t, "SendEmail", mock.Anything)
 	mockSESclient.AssertExpectations(t)
@@ -30,10 +33,26 @@ func Test_SendEmailVerificationMail(t *testing.T) {
 
 func Test_SendLoginTwoFaCode(t *testing.T) {
 	mockSESclient := mocks.NewSESClient(t)
-	SetSESClient(mockSESclient)
+	aws.SetSESClient(mockSESclient)
 	mockSESclient.On("SendEmail", mock.Anything).Return(&ses.SendEmailOutput{}, nil)
-	err := SendEmailVerificationMail("user@example.com", "123456")
+	err := aws.SendEmailVerificationMail("user@example.com", "123456")
 	assert.Nil(t, err)
+	mockSESclient.AssertCalled(t, "SendEmail", mock.Anything)
+	mockSESclient.AssertExpectations(t)
+}
+
+func Test_notOk(t *testing.T) {
+	mockSESclient := mocks.NewSESClient(t)
+	aws.SetSESClient(mockSESclient)
+
+	var sesErr awserr.Error
+	sesErr = test.SesError{
+		CodeData: ses.ErrCodeMessageRejected,
+	}
+
+	mockSESclient.On("SendEmail", mock.Anything).Return(&ses.SendEmailOutput{}, sesErr)
+	err := aws.SendEmailVerificationMail("user@example.com", "123456")
+	assert.NotNil(t, err)
 	mockSESclient.AssertCalled(t, "SendEmail", mock.Anything)
 	mockSESclient.AssertExpectations(t)
 }
