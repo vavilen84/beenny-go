16ddfa34036f575b04684f2d4fdf9bd1c4577309
Vladimir Teplov <vladimir.teplov@gmail.com>
2024-02-25 12:11:21 +0200

    add dto register tests

--- server/dto/dto_register_test.go
+++ server/dto/dto_register_test.go
@@ -4,30 +4,31 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/vavilen84/beenny-go/constants"
+	"github.com/vavilen84/beenny-go/helpers"
 	"github.com/vavilen84/beenny-go/validation"
-	"log"
 	"testing"
 )
 
 func Test_Register_notOk_1(t *testing.T) {
 	u := Register{}
-	err := validation.ValidateByScenario(constants.ScenarioRegister, u)
-	v, ok := err.(validation.Errors)
-	if !ok {
-		log.Fatalln("can not assert validation.Errors")
+	errs := validation.ValidateByScenario(constants.ScenarioRegister, u)
+
+	mustHaveErrors := []string{
+		fmt.Sprintf(constants.EmailErrorMsg, "Email"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "FirstName"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "LastName"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "Photo"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "Gender"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "Timezone"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "Birthday"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "AgreeTerms"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "ConfirmPassword"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "Password"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "CurrentCountry"),
+		fmt.Sprintf(constants.RequiredErrorMsg, "CountryOfBirth"),
 	}
-	assert.Equal(t, fmt.Sprintf(constants.EmailErrorMsg), v["Email"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "FirstName"), v["FirstName"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "LastName"), v["LastName"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "Photo"), v["Photo"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "Gender"), v["Gender"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "Timezone"), v["Timezone"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "Birthday"), v["Birthday"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "AgreeTerms"), v["AgreeTerms"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "ConfirmPassword"), v["ConfirmPassword"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "Password"), v["Password"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "CurrentCountry"), v["CurrentCountry"][0].Message)
-	assert.Equal(t, fmt.Sprintf(constants.RequiredErrorMsg, "CountryOfBirth"), v["CountryOfBirth"][0].Message)
+	ok := helpers.AllErrorsExist(mustHaveErrors, errs)
+	assert.True(t, ok)
 }
 
 func Test_Register_ok(t *testing.T) {
@@ -45,6 +46,6 @@ func Test_Register_ok(t *testing.T) {
 		ConfirmPassword: "12345678lT*",
 		Photo:           "/2024/01/23/s09d8fs09dfu.jpg",
 	}
-	err := validation.ValidateByScenario(constants.ScenarioRegister, u)
-	assert.Nil(t, err)
+	errs := validation.ValidateByScenario(constants.ScenarioRegister, u)
+	assert.Nil(t, errs)
 }
