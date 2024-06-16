8b20e59bef248ed494446644f778e5ec84bb31b2
vavilen84 <vladimir.teplov@gmail.com>
2024-06-16 16:33:14 +0300

    add test files & methods

--- server/handlers/base_test.go
+++ server/handlers/base_test.go
@@ -1,207 +1,79 @@
 package handlers_test
 
 import (
-	"bytes"
-	"encoding/json"
-	"github.com/stretchr/testify/assert"
-	"github.com/vavilen84/beenny-go/auth"
-	"github.com/vavilen84/beenny-go/dto"
+	"github.com/joho/godotenv"
+	"github.com/vavilen84/beenny-go/helpers"
 	"github.com/vavilen84/beenny-go/models"
 	"github.com/vavilen84/beenny-go/store"
-	"github.com/vavilen84/beenny-go/test"
 	"gorm.io/gorm"
-	"io"
 	"log"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+	"os"
 )
 
-type TestRegisterResp struct {
-	Status     int                 `json:"status"`
-	Data       interface{}         `json:"data"`
-	Error      string              `json:"error"`
-	Errors     map[string][]string `json:"errors"`
-	FormErrors map[string][]string `json:"formErrors"`
+var (
+	testAppInited = false
+)
+
+func loadEnv() (err error) {
+	// we run test from current folder
+	if err := godotenv.Load(".env"); err != nil {
+		// we run test from root folder
+		err = godotenv.Load("../.env")
+		if err != nil {
+			panic("can not load env file")
+		}
+	}
+	return err
 }
 
-type TestTwoFaLoginFirstResp struct {
-	Status     int                 `json:"status"`
-	Data       interface{}         `json:"data"`
-	Error      string              `json:"error"`
-	Errors     map[string][]string `json:"errors"`
-	FormErrors map[string][]string `json:"formErrors"`
+func afterEachTest(conn *gorm.DB) {
+	sess, _ := conn.DB()
+	sess.Close()
 }
 
-type TestTwoFaLoginSecondRespDataToken struct {
-	Token string `json:"token"`
+func beforeEachTest() {
+	if !testAppInited {
+		err := loadEnv()
+		if err != nil {
+			panic(err)
+		}
+		testAppInited = true
+	}
+	err := clearTestDb()
+	if err != nil {
+		panic(err)
+	}
+	migrate()
 }
 
-type TestTwoFaLoginSecondResp struct {
-	Status     int                               `json:"status"`
-	Data       TestTwoFaLoginSecondRespDataToken `json:"data"`
-	Error      string                            `json:"error"`
-	Errors     map[string][]string               `json:"errors"`
-	FormErrors map[string][]string               `json:"formErrors"`
-}
-
-func verifyEmail(t *testing.T, ts *httptest.Server, u models.User) {
-	//url := ts.URL + "/api/v1/security/verify-email?token=" + u.EmailTwoFaCode
-	//req, err := http.NewRequest(http.MethodGet, url, nil)
-	//if err != nil {
-	//	t.Fatalf("Failed to create request: %v", err)
-	//}
-	//
-	//res, err := http.DefaultClient.Do(req)
-	//if err != nil {
-	//	t.Fatalf("Failed to send request: %v", err)
-	//}
-	//defer res.Body.Close()
-	//
-	//responseBody, err := io.ReadAll(res.Body)
-	//if err != nil {
-	//	t.Fatalf("Error reading response body: %v", err)
-	//}
-	//
-	//registerResp := dto.Response{}
-	//err = json.Unmarshal(responseBody, &registerResp)
-	//if err != nil {
-	//	t.Fatalf("Error reading response body: %v", err)
-	//}
-	//
-	//if res.StatusCode != http.StatusOK {
-	//	t.Errorf("Expected status code %d but got %d", http.StatusOK, res.StatusCode)
-	//}
-	//
-	//assert.Equal(t, registerResp.Status, http.StatusOK)
-	//assert.Empty(t, registerResp.Error)
-	//assert.Empty(t, registerResp.Errors)
-	//assert.Empty(t, registerResp.FormErrors)
-}
-
-func loginUser(t *testing.T, ts *httptest.Server) string {
-	twoFaLoginFirstStep(t, ts)
-	jwtTok := twoFaLoginSecondStep(t, ts)
-	return jwtTok
-}
-
-func twoFaLoginSecondStep(t *testing.T, ts *httptest.Server) (jwtToken string) {
+func clearTestDb() error {
+	store.InitTestDB()
 	db := store.GetDB()
-	u, err := models.FindUserByEmail(db, test.TestUserEmail)
+	db.Exec("DROP DATABASE beenny_test")
+	err := db.Exec("CREATE DATABASE beenny_test").Error
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-
-	body := dto.TwoFaLoginStepTwo{
-		EmailTwoFaCode: u.EmailTwoFaCode,
-	}
-	bodyBytes, err := json.Marshal(body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req, err := http.NewRequest(http.MethodPost, ts.URL+"/api/v1/security/two-fa-login-step-two", bytes.NewReader(bodyBytes))
-	if err != nil {
-		t.Fatalf("Failed to create request: %v", err)
-	}
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatalf("Failed to send request: %v", err)
-	}
-	defer res.Body.Close()
-
-	responseBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	twoFaLoginSecondStep := TestTwoFaLoginSecondResp{}
-	err = json.Unmarshal(responseBody, &twoFaLoginSecondStep)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code %d but got %d", http.StatusOK, res.StatusCode)
-	}
-
-	assert.Equal(t, twoFaLoginSecondStep.Status, http.StatusOK)
-	assert.NotEmpty(t, twoFaLoginSecondStep.Data.Token)
-	assert.Empty(t, twoFaLoginSecondStep.Error)
-	assert.Empty(t, twoFaLoginSecondStep.Error)
-	assert.Empty(t, twoFaLoginSecondStep.Errors)
-	assert.Empty(t, twoFaLoginSecondStep.FormErrors)
-
-	return twoFaLoginSecondStep.Data.Token
+	return err
 }
 
-func twoFaLoginFirstStep(t *testing.T, ts *httptest.Server) {
-
-	body := dto.TwoFaLoginStepOne{
-		Email:    test.TestUserEmail,
-		Password: test.TestUserPassword,
+func getAppPath() string {
+	appPath := os.Getenv("APP_ROOT_PATH")
+	if appPath == "" {
+		panic("APP_ROOT_PATH env var is empty")
 	}
-	bodyBytes, err := json.Marshal(body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req, err := http.NewRequest(http.MethodPost, ts.URL+"/api/v1/security/two-fa-login-step-one", bytes.NewReader(bodyBytes))
-	if err != nil {
-		t.Fatalf("Failed to create request: %v", err)
-	}
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatalf("Failed to send request: %v", err)
-	}
-	defer res.Body.Close()
-
-	responseBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	twoFaLoginStepFirst := TestTwoFaLoginFirstResp{}
-	err = json.Unmarshal(responseBody, &twoFaLoginStepFirst)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("Expected status code %d but got %d", http.StatusOK, res.StatusCode)
-	}
-
-	assert.Equal(t, twoFaLoginStepFirst.Status, http.StatusOK)
-	assert.Empty(t, twoFaLoginStepFirst.Data)
-	assert.Empty(t, twoFaLoginStepFirst.Error)
-	assert.Empty(t, twoFaLoginStepFirst.Error)
-	assert.Empty(t, twoFaLoginStepFirst.Errors)
-	assert.Empty(t, twoFaLoginStepFirst.FormErrors)
+	return appPath
 }
 
-func checkToken(t *testing.T, db *gorm.DB, token string) *models.User {
-	isValid, err := auth.VerifyJWT(db, []byte(token))
-	if err != nil || token == "" || !isValid {
-		log.Fatalln(err)
-	}
-
-	jwtPayload, err := auth.ParseJWTPayload([]byte(token))
+func migrate() {
+	store.InitDB()
+	db := store.GetDB()
+	err := models.CreateMigrationsTableIfNotExists(db)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
-	assert.NotEmpty(t, jwtPayload.JWTInfoId)
-
-	jwtInfo, err := models.FindJWTInfoById(db, jwtPayload.JWTInfoId)
+	err = models.MigrateUp(db, helpers.GetMigrationsFolder())
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
-
-	userByJWTInfo, err := models.FindUserById(db, jwtInfo.UserId)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	return userByJWTInfo
 }
